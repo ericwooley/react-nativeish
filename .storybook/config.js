a05e894bf33049a9edb5cf4c59d4fddcc4aaa55c
Eric Wooley <eric.wooley@rosetta.com>
2017-04-03 16:02:42 -0700

    added storyshots

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,7 +2,6 @@ import { configure } from '@kadira/storybook';
 
 function loadStories() {
   require('../app/stories');
-  // You can require as many stories as you need.
 }
 
 configure(loadStories, module);
