877ff7146db8918776d6145ad91b9f886ed1f79f
Eric Wooley <eric.wooley@rosetta.com>
2017-04-03 15:45:03 -0700

    added storybooks

--- app/components/stars/stars.story.js
+++ app/components/stars/stars.story.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import { storiesOf, action } from '@kadira/storybook'
+import { storiesOf, action } from '@kadira/react-native-storybook'
 import Stars from './stars'
 storiesOf('Stars', module)
-  .add('with no onClick', () => (
-    <Stars />
+  .add('with positive count', () => (
+    <Stars starCount={12} onPress={action('get stars click')} />
   ))
-  .add('with count', () => (
-    <Stars starCount={12} />
-  ))
-  .add('with onClick', () => (
-    <Stars onClick={action('get stars click')} />
-  ))
-  .add('with onClick and count', () => (
-    <Stars starCount={47} onClick={action('get stars click')} />
+  .add('with negative count', () => (
+    <Stars starCount={-1} onPress={action('get stars click')} />
   ))
--- app/stories.js
+++ app/stories.js
@@ -1,3 +1,1 @@
-import './modules/chart1/chart1.story'
-import './modules/reduxModule/reduxModule.story'
 import './components/stars/stars.story'
