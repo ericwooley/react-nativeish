365395c2f17e9f7d375d31bbb523ed349138be46
Eric Wooley <ericwooley@gmail.com>
2017-04-12 12:10:54 -0700

    fixed coverage issues

--- .githooks/pre-commit/test.js
+++ .githooks/pre-commit/test.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node 
 var shell = require('shelljs')
 
-if (shell.exec('yarn test').code !== 0) {
+if (shell.exec('yarn test:coverage').code !== 0) {
   shell.echo('You cannot commit with broken tests');
   shell.exit(1);
 }
--- src/redux/sagas/stars/starsSaga.test.js
+++ src/redux/sagas/stars/starsSaga.test.js
@@ -1,8 +1,21 @@
-import {getStars} from './starsSaga'
-import { call } from 'redux-saga/effects'
+import {getStars, actions} from './starsSaga'
+import { call, put } from 'redux-saga/effects'
 import axios from 'axios'
 
 it('should call the api', () => {
   const iterator = getStars()
   expect(iterator.next().value).toEqual(call(axios.get, 'https://api.github.com/repos/ericwooley/react-native-redux-jest-starter-kit'))
+})
+
+it('should call the api', () => {
+  const iterator = getStars()
+  iterator.next()
+  expect(iterator.next({data: {stargazers_count: 32}}).value).toEqual(put(actions.FETCH_STARS_SUCCESS(32)))
+})
+
+it('should handle errors', () => {
+  const iterator = getStars()
+  iterator.next()
+  const error = new Error('test error')
+  expect(iterator.throw(error).value).toEqual(put(actions.FETCH_STARS_FAILURE(error)))
 })
